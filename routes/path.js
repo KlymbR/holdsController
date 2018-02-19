e3fa85e3c52eb0e747cdf55de3efbfc1a1c76618
Avel Docquin <avel.docquin@epitech.eu>
2018-02-19 18:33:21 +0300

    Supprimer une voie et une prise
    Ajoute de la suppression d'une voie et d'une prise. Documentation mise à jour.

--- routes/path.js
+++ routes/path.js
@@ -38,8 +38,8 @@ router.get('/path', function (req, se) {
 
 // update the free state of a path and change the free state of grips
 router.post('/path/free', function (req, se) {
-  Path.findOneAndUpdate({
-    path_id: req.body.path_id},
+  Path.findOneAndUpdate(
+    {path_id: req.body.path_id},
     {'$set': {'path_free': req.body.path_free}})
     .exec(function (err, res) {
       if (err) {
@@ -47,32 +47,55 @@ router.post('/path/free', function (req, se) {
         se.status(500).send(err)
         return err
       }
-      for (let i = 0; i !== res.grips.length; i++) {
-        if (req.body.path_free === true) {
-          res.grips[i].grip_on = false
-        } else {
-          res.grips[i].grip_on = true
+      if (!res) {
+        se.sendStatus(204)
+      } else {
+        for (let i = 0; i !== res.grips.length; i++) {
+          if (req.body.path_free === true) {
+            res.grips[i].grip_on = false
+          } else {
+            res.grips[i].grip_on = true
+          }
+          Path.findOneAndUpdate(
+            {path_id: req.body.path_id},
+            {'$set': {'grips': res.grips}})
+            .exec(function (err) {
+              if (err) {
+                console.log(err)
+                se.status(500).send(err)
+                return err
+              }
+            })
         }
-        Path.findOneAndUpdate({
-          path_id: req.body.path_id},
-          {'$set': {'grips': res.grips}})
-          .exec(function (err) {
-            if (err) {
-              console.log(err)
-              se.status(500).send(err)
-              return err
-            }
-          })
+        se.sendStatus(200)
       }
-      se.sendStatus(200)
+    })
+})
+
+// delete a path
+router.post('/path/delete', function (req, se) {
+  Path.findOne(
+    {path_id: req.body.path_id})
+    .exec(function (err, res) {
+      if (err) {
+        console.log(err)
+        se.status(500).send(err)
+        return err
+      }
+      if (!res) {
+        se.sendStatus(204)
+      } else {
+        res.remove()
+        se.sendStatus(200)
+      }
     })
 })
 
 // get the object of a grip
 router.get('/grip', function (req, se) {
   var nb = parseInt(req.query.grip_id)
-  Path.findOne({
-    path_id: req.query.path_id},
+  Path.findOne(
+    {path_id: req.query.path_id},
     {'grips': {$elemMatch: {grip_id: nb}}}, { 'grips.$': 1 })
     .exec(function (err, res) {
       if (err) {
@@ -90,16 +113,39 @@ router.get('/grip', function (req, se) {
 
 // update grip data
 router.post('/grip', function (req, se) {
-  Path.findOneAndUpdate({
-    path_id: req.body.path_id, 'grips.grip_id': req.body.grip_id},
+  Path.findOneAndUpdate(
+    {path_id: req.body.path_id, 'grips.grip_id': req.body.grip_id},
     {'$set': {'grips.$.grip_on': req.body.grip_on, 'grips.$.grip_data': req.body.grip_data}})
-    .exec(function (err) {
+    .exec(function (err, res) {
       if (err) {
         console.log(err)
         se.status(500).send(err)
         return err
       }
-      se.sendStatus(200)
+      if (!res) {
+        se.sendStatus(204)
+      } else {
+        se.sendStatus(200)
+      }
+    })
+})
+
+// delete a grip
+router.post('/grip/delete', function (req, se) {
+  Path.findOneAndUpdate(
+    {path_id: req.body.path_id, 'grips.grip_id': req.body.grip_id},
+    {'$set': {'grips.$': null}})
+    .exec(function (err, res) {
+      if (err) {
+        console.log(err)
+        se.status(500).send(err)
+        return err
+      }
+      if (!res) {
+        se.sendStatus(204)
+      } else {
+        se.sendStatus(200)
+      }
     })
 })
 
